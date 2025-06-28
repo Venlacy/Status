54d6248d5ac8d056390944d638eec2ee07fa0d85
Venlacy <120259055+Venlacy@users.noreply.github.com>
2025-06-28 18:31:55 +0800

    Update web.js

--- web.js
+++ web.js
@@ -1,7 +1,43 @@
-// 网站列表数据
-var websites = [
+// 网站数据
+const websites = [
     {
-        name: "Home",
-        url: "https://venlacy.top/"
+        name: "GitHub",
+        url: "https://github.com"
+    },
+    {
+        name: "Google",
+        url: "https://google.com"
+    },
+    {
+        name: "MDN Web Docs",
+        url: "https://developer.mozilla.org"
+    },
+    {
+        name: "Stack Overflow",
+        url: "https://stackoverflow.com"
+    },
+    {
+        name: "百度",
+        url: "https://baidu.com",
+        isChinese: true
+    },
+    {
+        name: "知乎",
+        url: "https://zhihu.com",
+        isChinese: true
+    },
+    {
+        name: "哔哩哔哩",
+        url: "https://bilibili.com",
+        isChinese: true
+    },
+    {
+        name: "淘宝",
+        url: "https://taobao.com",
+        isChinese: true
+    },
+    {
+        name: "示例离线网站",
+        url: "https://this-website-does-not-exist-123456.com"
     }
 ];
