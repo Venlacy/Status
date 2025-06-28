54932128aed74150f02e58b2836d641584a8e9aa
Venlacy <120259055+Venlacy@users.noreply.github.com>
2025-06-28 18:35:25 +0800

    Update web.js

--- web.js
+++ web.js
@@ -9,35 +9,20 @@ const websites = [
         url: "https://google.com"
     },
     {
-        name: "MDN Web Docs",
-        url: "https://developer.mozilla.org"
-    },
-    {
-        name: "Stack Overflow",
-        url: "https://stackoverflow.com"
-    },
-    {
-        name: "百度",
-        url: "https://baidu.com",
-        isChinese: true
-    },
-    {
-        name: "知乎",
-        url: "https://zhihu.com",
-        isChinese: true
-    },
-    {
         name: "哔哩哔哩",
         url: "https://bilibili.com",
         isChinese: true
     },
     {
-        name: "淘宝",
-        url: "https://taobao.com",
-        isChinese: true
+        name: "Venlacyの主页",
+        url: "https://home.venlacy.top"
     },
     {
-        name: "示例离线网站",
-        url: "https://this-website-does-not-exist-123456.com"
+        name: "Venlacyの网盘",
+        url: "https://g.venlacy.top"
+    },
+    {
+        name: "Venlacyの博客",
+        url: "https://venlacy.top/pan"
     }
 ];
