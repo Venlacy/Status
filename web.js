f8add6826b0674dac9ebedd7d81e7eabde2469dd
Venlacy <120259055+Venlacy@users.noreply.github.com>
2025-06-28 18:09:37 +0800

    Add files via upload

---
+++ web.js
@@ -0,0 +1,35 @@
+// 网站列表数据
+var websites = [
+    {
+        name: "GitHub",
+        url: "https://github.com"
+    },
+    {
+        name: "Google",
+        url: "https://google.com"
+    },
+    {
+        name: "MDN Web Docs",
+        url: "https://developer.mozilla.org"
+    },
+    {
+        name: "Stack Overflow",
+        url: "https://stackoverflow.com"
+    },
+    {
+        name: "百度",
+        url: "https://baidu.com"
+    },
+    {
+        name: "知乎",
+        url: "https://zhihu.com"
+    },
+    {
+        name: "哔哩哔哩",
+        url: "https://bilibili.com"
+    },
+    {
+        name: "示例离线网站",
+        url: "https://this-website-does-not-exist-123456.com"
+    }
+];
